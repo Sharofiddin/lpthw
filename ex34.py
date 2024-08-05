e4d1bc9610416229916b69ccf1e0a7c005f63767
sharofiddin <pardayev.sharofiddin@gmail.com>
2024-08-05 12:17:22 +0500

    lists

--- ex34.py
+++ ex34.py
@@ -1,12 +1,29 @@
 from sys import exit
 
-def gold_room():
-    print("This room is full of gold, How much do you take?")
+room_desc = {
+    'bear_room':'''There is a bear here.
+The bear has a bunch of honey.
+The fat bear is in front of another door.
+How are you going to move the bear?
+    ''',
+    'gold_room':'''This room is full of gold, How much do you take?
+''',
+    'cthulhu_room': '''Here you see the great evil Cthulhu.
+He, it, whatever stares at you and you go insane.
+Do you flee your life or aet your head?
+''',
+    'start_room': '''You are in a dark room.
+There is a door to your right and left.
+Which one do you take?
+'''
+}
 
+def gold_room():
+    print(room_desc['gold_room'])
     choice = input("> ")
-    if "0" in choice or "1" in choice:
+    try:
         how_much = int(choice)
-    else:
+    except ValueError:
         dead("Man, learn to type a number.")
 
     if how_much < 50:
@@ -16,12 +33,8 @@ def gold_room():
         dead("You greedy bastard!")
 
 def bear_room():
-    print("There is a bear here.")
-    print("The bear has a bunch of honey.")
-    print("The fat bear is in from of another door.")
-    print("How are you going to move the bear?")
+    print(room_desc['bear_room'])
     bear_moved = False
-
     while True:
         choice = input("> ")
 
@@ -39,10 +52,7 @@ def bear_room():
             print("I hot no idea what that means.")
 
 def cthulhu_room():
-    print("Here you see the great evil Cthulhu.")
-    print("He, it, whatever stares at you and you go insane.")
-    print("Do you flee your life or aet your head?")
-
+    print(room_desc['cthulhu_room'])
     choice = input("> ")
 
     if "flee" in choice:
@@ -57,10 +67,7 @@ def dead(why):
     exit(0)
 
 def start():
-    print("You are in a dark room.")
-    print("There is a door to your right and left.")
-    print("Which one do you take?")
-
+    print(room_desc['start_room'])
     choice = input("> ")
 
     if choice == "left":
